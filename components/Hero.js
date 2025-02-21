aa697a51f2057d5ef3fca7b3b30c276e5a5814e3
Python-Coderss <101146186+Python-Coderss@users.noreply.github.com>
2025-02-21 15:19:45 -0600

    Added coloured div and dashboard link

--- components/Hero.js
+++ components/Hero.js
@@ -1,1 +1,9 @@
 //test
+import Link from "next/link";
+
+const Hero = () => {
+
+    return <div style={"background-color=#ff0000;"}><Link href={"/"}>Dashboard</Link></div>
+};
+
+export default Hero;
